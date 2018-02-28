d5b28816f14fe461d9f00b76c208acc53415ae59
Temicka Brown <mekapleio@yahoo.com>
2018-02-27 19:57:10 -0500

    first commit, setting up ports and get

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+const port = 5000
+
+app.listen(port, () => {
+  console.log(`Server on port ${port}`) // using back ticks here.. is called a template string and allows you to use variables to concatenate. this is the es6 version
+})
