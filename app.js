3f4d206da3eeba642f0ae2eedef0a05fa9b8e6d2
Temicka Brown <mekapleio@yahoo.com>
2018-03-03 20:56:20 -0500

    completed CRUD app

--- app.js
+++ app.js
@@ -118,6 +118,14 @@ app.put('/ideas/:id', (req, res) => {
   })
 })
 
+// delete idea
+app.delete('/ideas/:id', (req, res) => {
+  Idea.remove({_id: req.params.id})
+      .then(() => {
+        res.redirect('/ideas')
+      })
+})
+
 const port = 5000
 
 app.listen(port, () => {
